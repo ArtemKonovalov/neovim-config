bbdb60184d78b903b3396884941eebbd05b36e22
Artem Konovalov <tmknvlv@gmail.com>
2026-01-04 12:45:40 +0000

    LSP configuration

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -3,3 +3,19 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+vim.diagnostic.config({
+  virtual_text = {
+    spacing = 2,
+    source = "if_any",
+    severity = { min = vim.diagnostic.severity.WARN },
+    format = function(d)
+      return d.message:gsub("\n", " ")
+    end,
+  },
+
+  signs = true,
+  underline = true,
+  severity_sort = true,
+  update_in_insert = false,
+})
